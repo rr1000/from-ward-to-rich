fedb41459025bab231d95b57e6da83cabd75cbf9
rr1000 <rrichrs@gmail.com>
2015-08-23 17:38:07 -0700

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fromWardToRichApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fromWardToRichApp
+ */
+angular.module('fromWardToRichApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fromWardToRichApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fromWardToRichApp
+ */
+angular.module('fromWardToRichApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
